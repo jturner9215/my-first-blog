f4e63cf2ba08ebbeb72d8fde5cd90c14aad9430b
jturner9215 <jturner9215@gmail.com>
2023-07-23 16:10:37 -0400

    This is such a pain :(

--- mysite/urls.py
+++ mysite/urls.py
@@ -14,17 +14,26 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.urls import path, include
-from . import views
 from django.contrib import admin
 
+from django.contrib.auth import views
+from .forms import PostForm
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('blog.urls')),
     path('', views.post_list, name='post_list'),
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
     path('post/new/', views.post_new, name='post_new'),
-    path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
+    path('drafts/', views.post_draft_list, name='post_draft_list'),
+    path('post/<pk>/publish/', views.post_publish, name='post_publish'),
+    path('post/<pk>/remove/', views.post_remove, name='post_remove'),
+    path('accounts/login/', views.LoginView.as_view(), name='login'),
+    path('admin/', admin.site.urls),
+    path('', include('blog.urls')),
+    path('accounts/logout/', views.LogoutView.as_view(next_page='/'), name='logout'),
+    path('post/<int:pk>/comment/', views.add_comment_to_post, name='add_comment_to_post'),
+    path('comment/<int:pk>/approve/', views.comment_approve, name='comment_approve'),
+    path('comment/<int:pk>/approve/', views.comment_approve, name='comment_approve'),
+
 ]
 
 
